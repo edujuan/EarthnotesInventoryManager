15d483a87226df77eb0f440c64626f5c3da86941
edujuan <69489757+edujuan@users.noreply.github.com>
2020-08-10 20:32:48 -0400

    Add files via upload

---
+++ BTM495 - Copy/BTM495/AvailableInventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTM495
+{
+    public partial class AvailableInventory : Form
+    {
+        public AvailableInventory()
+        {
+            InitializeComponent();
+        }
+        private void AvailableInventory_Load(object sender, EventArgs e)
+        {
+            DataAccess gridViewSource = new DataAccess();
+            
+            ProductsDataGridView.DataSource = gridViewSource.GetProducts();
+        }
+    }
+}
